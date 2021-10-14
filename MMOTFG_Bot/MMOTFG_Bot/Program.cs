6a6cfbae194d077706bc2c2afbc06b8a7e555bad
Pj <47722275+Pj6595@users.noreply.github.com>
2021-10-14 13:57:34 +0200

    Configuración inicial del bot

---
+++ MMOTFG_Bot/MMOTFG_Bot/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults;
+
+namespace MMOTFG_Bot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+			var botClient = new TelegramBotClient("1985137093:AAFk7-_Zyc2lSijP5diw2ghWPvmGVHKbB4E");
+			var me = await botClient.GetMeAsync();
+			Console.WriteLine("Hello World! I am user " + me.Id + " and my name is " + me.FirstName);
+
+            using var cts = new CancellationTokenSource();
+
+            botClient.StartReceiving(new DefaultUpdateHandler(HandleUpdateAsync, HandleErrorAsync), cts.Token);
+
+            Console.WriteLine($"Start listening for @{me.Username}");
+            Console.ReadLine();
+
+            cts.Cancel();
+        }
+
+		static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+		{
+			var handler = update.Type switch
+			{
+				// UpdateType.Unknown:
+				// UpdateType.ChannelPost:
+				// UpdateType.EditedChannelPost:
+				// UpdateType.ShippingQuery:
+				// UpdateType.PreCheckoutQuery:
+				// UpdateType.Poll:
+				//UpdateType.Message => BotOnMessageReceived(botClient, update.Message),
+				//UpdateType.EditedMessage => BotOnMessageReceived(botClient, update.EditedMessage),
+				//UpdateType.CallbackQuery => BotOnCallbackQueryReceived(botClient, update.CallbackQuery),
+				//UpdateType.InlineQuery => BotOnInlineQueryReceived(botClient, update.InlineQuery),
+				//UpdateType.ChosenInlineResult => BotOnChosenInlineResultReceived(botClient, update.ChosenInlineResult),
+				//_ => UnknownUpdateHandlerAsync(botClient, update)
+			};
+
+			try
+			{
+				await handler;
+			}
+			catch (Exception exception)
+			{
+				await HandleErrorAsync(botClient, exception, cancellationToken);
+			}
+		}
+
+		static async Task BotOnInlineQueryReceived(ITelegramBotClient botClient, InlineQuery query)
+		{
+			InlineQueryResultBase[] results = {
+					//// displayed result
+					//new InlineQueryResultArticle(
+					//	id: "0",
+					//	title: "dumbify your message!",
+					//	inputMessageContent: new InputTextMessageContent(
+					//		DumbifyText(query.Query)
+					//	)
+					//)
+				};
+
+			await botClient.AnswerInlineQueryAsync(
+				inlineQueryId: query.Id,
+				results: results,
+				isPersonal: true,
+				cacheTime: 0);
+		}
+
+		static async Task BotOnMessageReceived(ITelegramBotClient botClient, Message message)
+		{
+			var chatId = message.Chat.Id;
+			var senderName = message.From.FirstName;
+
+			Console.WriteLine("Received message: " + message.Text + " from " + senderName);
+
+			//await botClient.SendTextMessageAsync(chatId: chatId, text: DumbifyText(message.Text));
+		}
+
+		static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+		{
+			var errorMessage = exception switch
+			{
+				ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+				_ => exception.ToString()
+			};
+			Console.WriteLine(errorMessage);
+			return Task.CompletedTask;
+		}
+	}
+}
