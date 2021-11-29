cd1a4210747ce5b9be390ca672bf3e167ac85451
Pj <47722275+Pj6595@users.noreply.github.com>
2021-11-29 13:42:01 +0100

    Mapa navegable sencillito yea
    Co-Authored-By: Miguel Mur Cortés <mmur@ucm.es>

---
+++ MMOTFG_Bot/MMOTFG_Bot/Map.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MMOTFG_Bot
+{
+    public enum Direction { North, South, East, West, NUM_DIRECTIONS };
+    public enum ItemID { PenDrive, NUM_ITEMS};
+
+    static class ItemInfo
+    {
+        private static Event[] onConsumeEvents;
+        private static Event[] onInspectEvents;
+        private static string[] itemNames;
+
+        public static void Init()
+        {
+            onConsumeEvents = new Event[(int)ItemID.NUM_ITEMS];
+            onInspectEvents = new Event[(int)ItemID.NUM_ITEMS];
+            itemNames = new string[(int)ItemID.NUM_ITEMS];
+        }
+        public static void consume(ItemID id) { onConsumeEvents[(int)id].trigger(); }
+        public static void inspect(ItemID id) { onInspectEvents[(int)id].trigger();  }
+        public static string getItemName(ItemID id) { return itemNames[(int)id]; }
+
+        public static void addConsumeEvent(ItemID id, Event e) {
+            onConsumeEvents[(int)id] = e;
+        }
+
+        public static void addInspectEvent(ItemID id, Event e)
+        {
+            onInspectEvents[(int)id] = e;
+        }
+
+        public static void setItemName(ItemID id, string name)
+        {
+            itemNames[(int)id] = name;
+        }
+    }
+
+    static class Inventory
+    {
+        static int[] items;
+
+        public static void Init()
+        {
+            items = new int[(int)ItemID.NUM_ITEMS];
+            ItemInfo.Init();
+        }
+
+        public static void addItem(ItemID item, int num = 1)
+        {
+            items[(int)item] += num;
+        }
+
+        public static bool useItem(ItemID id)
+        {
+            if (items[(int)id] > 0)
+            {
+                items[(int)id] -= 1;
+                ItemInfo.consume(id);
+                return true;
+            }
+            else return false;
+
+            if (items[(int)id] < 0) items[(int)id] = 0;
+        }
+    }
+
+    class MapNode
+    {
+        public string name;
+        public string description;
+        public MapNode[] connectedNodes = new MapNode[(int)Direction.NUM_DIRECTIONS];
+        public Event onArriveEvent, onExitEvent, onLookEvent;
+
+        public MapNode()
+        {
+            //lol
+        }
+
+        public void onArrive()
+        {
+            if (onArriveEvent != null) onArriveEvent.trigger();
+        }
+
+        public void onExit()
+        {
+            if (onExitEvent != null) onExitEvent.trigger();
+        }
+
+        public void onLook()
+        {
+            if  (onLookEvent != null) onLookEvent.trigger();
+        }
+
+        public void connectNode(MapNode node, Direction dir)
+        {
+            connectedNodes[(int)dir] = node;
+        }
+
+        public bool isConnected(Direction dir)
+        {
+            return (connectedNodes[(int)dir] != null);
+        }
+
+        public MapNode getConnectedNode(Direction dir){
+            return connectedNodes[(int)dir];
+        }
+    }
+
+    class Map
+    {
+        Dictionary<string, MapNode>  nodes;
+        int nNodes = 0;
+        MapNode currentNode;
+
+        public Map()
+        {
+            nodes = new Dictionary<string, MapNode>();
+        }
+
+        public void addNode(string name, MapNode node)
+        {
+            nodes[name] = node;
+        }
+
+        public void navigate(Direction dir)
+        {
+            if(currentNode.isConnected(dir))
+            {
+                currentNode.onExit();
+                currentNode = currentNode.getConnectedNode(dir);
+                currentNode.onArrive();
+            }
+        }
+
+        public void connectNode(string origin, string destination, Direction dir)
+        {
+            if (!nodes.ContainsKey(origin) || !nodes.ContainsKey(destination)) throw new Exception("No existen los nodos");
+            nodes[origin].connectNode(nodes[destination], dir);
+        }
+
+        public void setPosition(string node)
+        {
+            currentNode = nodes[node];
+            currentNode.onArrive();
+        }
+
+        public void lookAround()
+        {
+            currentNode.onLook();
+        }
+    }
+
+    class Event
+    {
+        private List<Action> actions;
+
+        public Event() {
+            actions = new List<Action>();
+        }
+
+        public void addAction(Action a)
+        {
+            actions.Add(a);
+        }
+
+        public void trigger()
+        {
+            for  (int k = 0; k < actions.Count; k++) actions[k].execute();
+        }
+    }
+
+    abstract class Action
+    {
+        public abstract void execute();
+    }
+
+    class DescriptorAction : Action
+    {
+        string text;
+
+        public DescriptorAction(string t) { text = t;  }
+
+        public override void execute()
+        {
+            Console.WriteLine(text);
+        }
+    }
+
+    class GiveItemAction : Action
+    {
+        ItemID item;
+
+        public GiveItemAction(ItemID it) { 
+            item = it; 
+        }
+
+        public override void execute()
+        {
+            Console.WriteLine("Has obtenido el item " + ItemInfo.getItemName(item) + ".");
+            Inventory.addItem(item);
+        }
+    }
+
+    class ConsumeItemAction : Action
+    {
+        ItemID item;
+
+        public ConsumeItemAction(ItemID it) { 
+            item = it; 
+        }
+
+        public override void execute()
+        {
+            Console.WriteLine("Has usado el item " + ItemInfo.getItemName(item) + ".");
+            Inventory.useItem(item);
+        }
+    }
+}
--- MMOTFG_Bot/MMOTFG_Bot/Program.cs
+++ MMOTFG_Bot/MMOTFG_Bot/Program.cs
@@ -12,89 +12,157 @@ namespace MMOTFG_Bot
 {
     class Program
     {
-        static async Task Main(string[] args)
+		
+		static void Main(string[] args)
         {
-			var botClient = new TelegramBotClient("1985137093:AAFk7-_Zyc2lSijP5diw2ghWPvmGVHKbB4E");
-			var me = await botClient.GetMeAsync();
-			Console.WriteLine("Hello World! I am user " + me.Id + " and my name is " + me.FirstName);
+			Map mapa = new Map();
+			Inventory.Init();
 
-            using var cts = new CancellationTokenSource();
+			Event arriveEvent = new Event();
+			arriveEvent.addAction(new DescriptorAction("Nunca te gustó mucho la entrada a la facultad, pero depsués de tantos años ya te has acostumbrado."));
+			MapNode entranceNode = new MapNode();
+			entranceNode.onArriveEvent = arriveEvent;
+			mapa.addNode("entrance", entranceNode);
 
-            botClient.StartReceiving(new DefaultUpdateHandler(HandleUpdateAsync, HandleErrorAsync), cts.Token);
+			arriveEvent = new Event();
+			arriveEvent.addAction(new DescriptorAction("Al aula cinco ya no le queda ni un ápice del brillo que tenía en 2018. Te sientes triste solo de mirar a través de la puerta."));
+			MapNode aulaNode = new MapNode();
+			aulaNode.onArriveEvent = arriveEvent;
+			Event exitEvent = new Event();
+			exitEvent.addAction(new DescriptorAction("Al salir del aula cinco sientes al fantasma de Fede acechándote. Yikes dawg"));
+			aulaNode.onExitEvent = exitEvent;
+			mapa.addNode("aula5", aulaNode);
+			Event lookEvent = new Event();
+			lookEvent.addAction(new DescriptorAction("Encima de la mesa del profesor, observas que hay un pen-drive abandonado. Decides cogerlo"));
 
-            Console.WriteLine($"Start listening for @{me.Username}");
-            Console.ReadLine();
+			ItemInfo.setItemName(ItemID.PenDrive, "pendrive misterioso");
+			Event onUseEvent = new Event();
+			onUseEvent.addAction(new DescriptorAction("El pendrive contiene el examen de consolas con la solución de la última práctica. ¡Menudo éxito!"));
+			ItemInfo.addConsumeEvent(ItemID.PenDrive, onUseEvent);
 
-            cts.Cancel();
-        }
+			lookEvent.addAction(new GiveItemAction(ItemID.PenDrive));
+			aulaNode.onLookEvent = lookEvent;
 
-		static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
-		{
-			var handler = update.Type switch
-			{
-				// UpdateType.Unknown:
-				// UpdateType.ChannelPost:
-				// UpdateType.EditedChannelPost:
-				// UpdateType.ShippingQuery:
-				// UpdateType.PreCheckoutQuery:
-				// UpdateType.Poll:
-				//UpdateType.Message => BotOnMessageReceived(botClient, update.Message),
-				//UpdateType.EditedMessage => BotOnMessageReceived(botClient, update.EditedMessage),
-				//UpdateType.CallbackQuery => BotOnCallbackQueryReceived(botClient, update.CallbackQuery),
-				//UpdateType.InlineQuery => BotOnInlineQueryReceived(botClient, update.InlineQuery),
-				//UpdateType.ChosenInlineResult => BotOnChosenInlineResultReceived(botClient, update.ChosenInlineResult),
-				//_ => UnknownUpdateHandlerAsync(botClient, update)
-			};
+			mapa.connectNode("entrance", "aula5", Direction.North);
+			mapa.connectNode("aula5", "entrance", Direction.South);
 
-			try
-			{
-				await handler;
-			}
-			catch (Exception exception)
-			{
-				await HandleErrorAsync(botClient, exception, cancellationToken);
-			}
+			mapa.setPosition("entrance");
+
+			char c;
+            while (true)
+            {
+				c = (char)Console.Read();
+                switch (c)
+                {
+					case ('w'):
+						mapa.navigate(Direction.North);
+						break;
+					case ('s'):
+						mapa.navigate(Direction.South);
+						break;
+					case ('d'):
+						mapa.navigate(Direction.East);
+						break;
+					case ('a'):
+						mapa.navigate(Direction.West);
+						break;
+					case ('l'):
+						mapa.lookAround();
+						break;
+					case ('u'):
+                        if (!Inventory.useItem(ItemID.PenDrive))
+                        {
+							Console.WriteLine("Ibas a usar un objeto, pero llevas el bolsillo más vacío que la sección de calificaciones del campus virtual");
+                        }
+						break;
+				}
+            }
 		}
 
-		static async Task BotOnInlineQueryReceived(ITelegramBotClient botClient, InlineQuery query)
-		{
-			InlineQueryResultBase[] results = {
-					//// displayed result
-					//new InlineQueryResultArticle(
-					//	id: "0",
-					//	title: "dumbify your message!",
-					//	inputMessageContent: new InputTextMessageContent(
-					//		DumbifyText(query.Query)
-					//	)
-					//)
-				};
+ //       static async Task Main(string[] args)
+ //       {
+	//		var botClient = new TelegramBotClient("1985137093:AAFk7-_Zyc2lSijP5diw2ghWPvmGVHKbB4E");
+	//		var me = await botClient.GetMeAsync();
+	//		Console.WriteLine("Hello World! I am user " + me.Id + " and my name is " + me.FirstName);
 
-			await botClient.AnswerInlineQueryAsync(
-				inlineQueryId: query.Id,
-				results: results,
-				isPersonal: true,
-				cacheTime: 0);
-		}
+ //           using var cts = new CancellationTokenSource();
 
-		static async Task BotOnMessageReceived(ITelegramBotClient botClient, Message message)
-		{
-			var chatId = message.Chat.Id;
-			var senderName = message.From.FirstName;
+ //           botClient.StartReceiving(new DefaultUpdateHandler(HandleUpdateAsync, HandleErrorAsync), cts.Token);
 
-			Console.WriteLine("Received message: " + message.Text + " from " + senderName);
+ //           Console.WriteLine($"Start listening for @{me.Username}");
+ //           Console.ReadLine();
 
-			//await botClient.SendTextMessageAsync(chatId: chatId, text: DumbifyText(message.Text));
-		}
+ //           cts.Cancel();
+ //       }
 
-		static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
-		{
-			var errorMessage = exception switch
-			{
-				ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
-				_ => exception.ToString()
-			};
-			Console.WriteLine(errorMessage);
-			return Task.CompletedTask;
-		}
+	//	static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+	//	{
+	//		var handler = update.Type switch
+	//		{
+	//			// UpdateType.Unknown:
+	//			// UpdateType.ChannelPost:
+	//			// UpdateType.EditedChannelPost:
+	//			// UpdateType.ShippingQuery:
+	//			// UpdateType.PreCheckoutQuery:
+	//			// UpdateType.Poll:
+	//			UpdateType.Message => BotOnMessageReceived(botClient, update.Message),
+	//			//UpdateType.EditedMessage => BotOnMessageReceived(botClient, update.EditedMessage),
+	//			//UpdateType.CallbackQuery => BotOnCallbackQueryReceived(botClient, update.CallbackQuery),
+	//			//UpdateType.InlineQuery => BotOnInlineQueryReceived(botClient, update.InlineQuery),
+	//			//UpdateType.ChosenInlineResult => BotOnChosenInlineResultReceived(botClient, update.ChosenInlineResult),
+	//			//_ => UnknownUpdateHandlerAsync(botClient, update)
+	//		};
+
+	//		try
+	//		{
+	//			await handler;
+	//		}
+	//		catch (Exception exception)
+	//		{
+	//			await HandleErrorAsync(botClient, exception, cancellationToken);
+	//		}
+	//	}
+
+	//	static async Task BotOnInlineQueryReceived(ITelegramBotClient botClient, InlineQuery query)
+	//	{
+	//		InlineQueryResultBase[] results = {
+	//				//// displayed result
+	//				//new InlineQueryResultArticle(
+	//				//	id: "0",
+	//				//	title: "dumbify your message!",
+	//				//	inputMessageContent: new InputTextMessageContent(
+	//				//		DumbifyText(query.Query)
+	//				//	)
+	//				//)
+	//			};
+
+	//		await botClient.AnswerInlineQueryAsync(
+	//			inlineQueryId: query.Id,
+	//			results: results,
+	//			isPersonal: true,
+	//			cacheTime: 0);
+	//	}
+
+	//	static async Task BotOnMessageReceived(ITelegramBotClient botClient, Message message)
+	//	{
+	//		var chatId = message.Chat.Id;
+	//		var senderName = message.From.FirstName;
+
+	//		Console.WriteLine("Received message: " + message.Text + " from " + senderName);
+
+	//		//await botClient.SendTextMessageAsync(chatId: chatId, text: DumbifyText(message.Text));
+	//	}
+
+	//	static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+	//	{
+	//		var errorMessage = exception switch
+	//		{
+	//			ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+	//			_ => exception.ToString()
+	//		};
+	//		Console.WriteLine(errorMessage);
+	//		return Task.CompletedTask;
+	//	}
+
 	}
 }
