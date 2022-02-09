4d5b17764ff091b4150bb20c1a5139be4acb1bf3
Andres Ruiz <andrui10@ucm.es>
2022-02-09 12:35:55 +0100

    Commit inicial del testeo con sql

---
+++ SQLTest/PruebaSQL/PruebaSQL/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PruebaSQL
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			//Server = tcp:localhost,Port; Initial Catalog = databaseName; "
+			//	  + "User ID =UserID;Password =Password";
+
+			//Referencia a las connectionStrings
+			//https://docs.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlconnection.connectionstring?view=dotnet-plat-ext-6.0
+
+			string connectionString =
+			"Server=tcp:localhost,1433;Initial Catalog=model;"
+			+ "User ID =SA;Password =contrasena1+"
+			+ "Timeout = 5";
+
+			// Provide the query string with a parameter placeholder.
+			string queryString =
+				"SELECT ProductID, UnitPrice, ProductName from dbo.products "
+					+ "WHERE UnitPrice > @pricePoint "
+					+ "ORDER BY UnitPrice DESC;";
+
+			// Specify the parameter value.
+			int paramValue = 5;
+
+			Console.WriteLine("Creando conexion");
+
+			// Create and open the connection in a using block. This
+			// ensures that all resources will be closed and disposed
+			// when the code exits.
+			using (SqlConnection connection =
+				new SqlConnection(connectionString))
+			{
+				// Create the Command and Parameter objects.
+				SqlCommand command = new SqlCommand(queryString, connection);
+				command.Parameters.AddWithValue("@pricePoint", paramValue);
+
+				// Open the connection in a try/catch block.
+				// Create and execute the DataReader, writing the result
+				// set to the console window.
+				try
+				{
+					connection.Open();
+					Console.WriteLine("Conectado");
+					SqlDataReader reader = command.ExecuteReader();
+					while (reader.Read())
+					{
+						Console.WriteLine("Leyendo");
+						Console.WriteLine("\t{0}\t{1}\t{2}",
+							reader[0], reader[1], reader[2]);
+					}
+					reader.Close();
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine("Error:");
+					Console.WriteLine(ex.Message);
+				}
+				Console.ReadLine();
+			}
+		}
+	}
+}
