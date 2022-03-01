db52e5406ea42ba37aaf5014308f2f6341d4994f
Auraum <aldomi04@ucm.es>
2022-03-01 01:47:33 +0100

    New info just dropped

--- MMOTFG_Bot/MMOTFG_Bot/InformationSystem.cs
+++ MMOTFG_Bot/MMOTFG_Bot/InformationSystem.cs
@@ -9,7 +9,11 @@ namespace MMOTFG_Bot
         static Dictionary<string, string> information = new Dictionary<string, string>
         {
             {"manuela", "Una avispa poco amigable pero poco peligrosa."},
-            {"tortazo", "Fuerza: 1.5, Coste de MP: 0" }
+            {"tortazo", "Fuerza: 1.5, Coste de MP: 0" },
+            {"overkill", "Fuerza: 100, Coste de MP: 100"},
+            {"peter", "Un ser misterioso. Ten cuidado con dejarle con poca vida..."},
+            {"mp", "El coste de maná para usar determinado ataque. El ataque básico siempre cuesta 0."},
+            {"/eat", "Comando que se usa para ingerir alimentos. No comas bebidas!!!1!"}
         };
 
         public static async void showInfo(long chatId, string key)
