f34cd4fb1c2c3ab689067761567519bf50eafcd4
Miguel Mur Cortés <mmur@ucm.es>
2022-03-26 13:45:21 +0100

    evento setCondition aun no funcional

---
+++ MMOTFG_Bot/MMOTFG_Bot/Events/eSetCondition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MMOTFG_Bot.Events
+{
+    /// <summary>
+    /// Sends a text message to the user.
+    /// </summary>
+    class eSetCondition : Event
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public bool SetAs
+        {
+            get;
+            set;
+        }
+
+        public async override void Execute(long chatId)
+        {
+            await TelegramCommunicator.SendText(chatId, Description);
+        }
+    }
+}
